29a81b4e3945bc36b6082f671781cacbfdefa168
Bartek Szatkowski <bulislaw@linux.com>
2016-08-18 19:44:54 +0100

    TEMP_OAK: Fixes: result rounding; key file location

--- temperature_oak.py
+++ temperature_oak.py
@@ -3,17 +3,17 @@
 from spyrk import SparkCloud
 
 class TemperatureOak(SparkCloud):
-    def __init__(self, devf=".temperature.oak"):
+    def __init__(self, devf="/root/runtime/homelib/.temperature.oak"):
         with open(devf) as f:
             self.name, self.token = f.readline()[:-1].split(":")
 
             SparkCloud.__init__(self, self.token)
 
     def read_temp(self):
-        return self.devices[self.name].temp_up
+        return round(self.devices[self.name].temp_up, 2)
 
     def read_hum(self):
-        return self.devices[self.name].hum_up
+        return round(self.devices[self.name].hum_up, 2)
 
 if __name__ == "__main__":
     oak = TemperatureOak()
